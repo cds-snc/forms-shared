8515b8c05c6f878f005c200d9baa611e22dd8cf5
Tim Arney <tim@line37.com>
2020-04-07 06:25:34 -0400

    initial styling

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
 import "./style.css";
-
-function classToggle() {
-  const nav = document.querySelector(".mobile-nav");
-  const arrow = document.querySelector(".mobile-arrow");
-  nav.classList.toggle("hidden");
-  arrow.classList.toggle("flip");
-}
-document.querySelector("#menu").addEventListener("click", classToggle);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,62 +1,57 @@
 module.exports = {
   theme: {
+    extend: {
+      spacing: {
+        "72": "18rem",
+        "84": "21rem",
+        "96": "24rem",
+      },
+    },
     container: {
-      center: true
+      center: true,
     },
     boxShadow: {
-      outline: "0 0 0 3px rgba(255, 191, 71, 1)"
-    },
-    maxWidth: {
-      "4xl": "53rem"
-    },
-    fontSize: {
-      xs: "1.3rem",
-      small: "1.6rem",
-      smaller: "1.9rem",
-      base: "2.0rem",
-      lg: "3.6rem",
-      xl: "3.8rem",
-      xxl: "6.5rem",
-      brand: "2.6rem"
+      outline: "0 0 0 3px rgba(255, 191, 71, 1)",
     },
     fontFamily: {
       sans: ["lato"],
-      body: ["Noto Sans"]
+      body: ["Noto Sans"],
     },
     screens: {
       sm: "640rem",
       md: "768px",
-      lg: "1024px"
+      lg: "1024px",
     },
     colors: {
       red: {
-        default: "#b10e1e"
+        default: "#b10e1e",
       },
       white: {
-        default: "#FFF"
+        default: "#FFF",
       },
       blue: {
-        lighter: "#B2E3FF",
         default: "#26374A",
-        selected: "#75b9e0"
+        100: "#B2E3FF",
+        200: "#DFF8FD",
+        300: "#4B98B2",
+        400: "#335075",
+        500: "#75b9e0",
       },
       gray: {
         default: "#EEE",
-        selected: "#e1e4e7"
+        selected: "#e1e4e7",
       },
       yellow: {
-        default: "#ffbf47"
+        default: "#ffbf47",
       },
       green: {
         default: "#00703C",
-        darker: "#002D18"
-      }
-    }
+        darker: "#002D18",
+      },
+      black: {
+        default: "#000",
+      },
+    },
   },
-  extend: {
-    lineHeight: {
-      'extra-tight': '0.9',
-    }
-  },
-  plugins: []
+  plugins: [require("@tailwindcss/custom-forms")],
 };
