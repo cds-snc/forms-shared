c3d7141c6cf49d76a61aaee2b5dd14203ec0e023
Tim Arney <tim@line37.com>
2020-05-06 05:34:26 -0400

    field styling

--- wordpress/wp-content/themes/canadian-digital-service/public/js/main.js
+++ wordpress/wp-content/themes/canadian-digital-service/public/js/main.js
@@ -1,8 +1,11 @@
-jQuery(document).ready(function($){
-    $(":text").addClass("input w-full lg:w-3/6");
-    $(":radio").addClass("text-blue form-radio h-6 w-6");
-    $(":checkbox").addClass("text-blue form-checkbox h-6 w-6");
-    $(".gfield").addClass("form-group");
-    $("[class^=gchoice]").addClass("inline-flex items-center");
-    $("[class^=gchoice] label").addClass("ml-3 text-lg");
+jQuery(document).ready(function ($) {
+  /*
+  $(":text").addClass("input w-full lg:w-3/6");
+  $(":radio").addClass("text-blue form-radio h-6 w-6");
+  $(":checkbox").addClass("text-blue form-checkbox h-6 w-6");
+  $(".gfield").addClass("form-group");
+
+  $("[class^=gchoice]").addClass("inline-flex items-center");
+  $("[class^=gchoice] label").addClass("ml-3 text-lg");
+  */
 });
