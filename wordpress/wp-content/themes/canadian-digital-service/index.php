ae48e14a5c037b1cb983fe0934692a8d900516de
Tim Arney <tim@line37.com>
2020-05-12 07:24:18 -0400

    re-add h1

--- wordpress/wp-content/themes/canadian-digital-service/index.php
+++ wordpress/wp-content/themes/canadian-digital-service/index.php
@@ -20,7 +20,7 @@ if ( have_posts() ) :
 	
 	while ( have_posts() ) :
 		the_post();
-		// the_title( '<h2 class="text-4xl font-bold pb-5 font-sans">', '</h2>' );
+		the_title( '<h1>', '</h1>' );
 		the_content();
 	endwhile; // End of the loop.
 	
