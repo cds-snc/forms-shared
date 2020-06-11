b57ceb76901b3f007d6b50a8815b0596d97fbdd4
Tim Arney <tim@line37.com>
2020-06-11 09:12:01 -0400

    cleanup

--- wordpress/wp-content/themes/canadian-digital-service/404.php
+++ wordpress/wp-content/themes/canadian-digital-service/404.php
@@ -6,20 +6,14 @@
  */
 get_header();
 ?>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e('Oops! That page can&rsquo;t be found.', 'canadian-digital-service'); ?></h1>
-				</header><!-- .page-header -->
+<div class="container mx-auto mb-10">
+	<header class="page-header">
+		<h1 class="page-title"><?php esc_html_e('Oops! That page can&rsquo;t be found.', 'canadian-digital-service'); ?></h1>
+	</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e('It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'canadian-digital-service'); ?></p>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+	<div class="page-content">
+		<p><?php esc_html_e('It looks like nothing was found at this location.', 'canadian-digital-service'); ?></p>
+	</div><!-- .page-content -->		
+</div>
 <?php
 get_footer();
--- wordpress/wp-content/themes/canadian-digital-service/public/js/editor.js
+++ wordpress/wp-content/themes/canadian-digital-service/public/js/editor.js
@@ -1,7 +1,5 @@
 
-let content = "<h2>Tutorials</h2>";
-content+= '<p><iframe width="560" height="315" src="https://www.youtube.com/embed/6aQiOX5eDxY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>';
-
+let content = "";
 
 jQuery(document).ready(function($){
   $("#no-fields").html("<div>"+content+"</div>").show();
@@ -16,9 +14,7 @@ jQuery(document).ready(function($){
   $("li input[data-type='captcha']").parent().remove();
   $("li input[data-type='consent']").parent().remove();
   $("li input[data-type='list']").parent().remove();
-
   $("li input[data-type='radio']").val("Multiple Choice");
-
   $("#add_post_fields").html("");
   $("#add_pricing_fields").html("");
 });
